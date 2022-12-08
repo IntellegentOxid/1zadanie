47dde24d9f59fd07a28a2c57eab70559a71fb775
revelations? <flfkflfkkf@gmail.com>
2022-12-08 11:37:49 +0200

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, ".1251");
+	double x, y, z;
+	cout << "Введіть діаметр x=";
+		cin >> x;
+		y = (3.14 * x);
+		z = (2 * x * 3.14);
+		cout << "Довжина кола у=" << y << endl;
+		cout << "Площа кола z=" << z << endl;
+		system("pause>>void");
+		return 0;
+}
